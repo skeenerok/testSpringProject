1b2818dd7eabdd8b7cbd5dfdd922605d18de1e70
Mykola Yerykalov <ierykalov@gmail.com>
2021-04-19 16:52:20 +0300

    fix paginator

--- src/main/java/com/example/servingwebcontent/service/MailSender.java
+++ src/main/java/com/example/servingwebcontent/service/MailSender.java
@@ -12,12 +12,18 @@ public class MailSender {
     private JavaMailSender mailSender;
     @Value("${spring.mail.username}")
     private String username;
+    @Value("${spring.mail.fakeMode}")
+    private boolean fakeMode;
+
     public void send(String mailTo, String subject, String message){
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(mailTo);
         mailMessage.setFrom(username);
         mailMessage.setSubject(subject);
         mailMessage.setText(message);
+        if(fakeMode){
+            return;
+        }
         mailSender.send(mailMessage);
     }
 }
